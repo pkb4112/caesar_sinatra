1b6d6bc8126d0f9983e326ebe5ef5ef0c336ca31
Peter Brandon <peterkbrandon@gmail.com>
2017-11-02 12:36:23 -0400

    Initial solution

--- caesar.rb
+++ caesar.rb
@@ -1,3 +1,25 @@
 require 'sinatra'
 require 'sinatra/reloader' if development?
 
+def encrypt(word,shift)
+  encrypted = word.split("").collect do |x| 
+    if x.match(/\w/)
+      val = x.ord + shift 
+      val = val.chr
+    else
+      val = x
+    end
+  end
+  encrypted = encrypted.inject{|x,y| x+y}
+  return encrypted
+end
+
+
+get '/' do 
+  word = params["word"]
+  shift = params["shift"].to_i
+
+  encrypted = encrypt(word,shift) if word
+
+  erb :index, :locals => {:encrypted => encrypted}
+end
